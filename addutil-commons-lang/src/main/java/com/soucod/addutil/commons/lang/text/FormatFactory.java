085c027783a71f5f6ebbb917d3f52d4dc38b1ad1
LuDaShi <wlty17173@gmail.com>
2022-07-12 15:16:06 +0800

    简化代码,第3版本

--- addutil-commons-lang/src/main/java/com/soucod/addutil/commons/lang/text/FormatFactory.java
+++ addutil-commons-lang/src/main/java/com/soucod/addutil/commons/lang/text/FormatFactory.java
@@ -16,9 +16,6 @@
  */
 package com.soucod.addutil.commons.lang.text;
 
-import java.text.Format;
-import java.util.Locale;
-
 /**
  * Format factory.
  *
@@ -28,18 +25,7 @@ import java.util.Locale;
  * FormatFactory</a> instead
  */
 @Deprecated
-public interface FormatFactory {
+public interface FormatFactory extends org.apache.commons.lang3.text.FormatFactory {
 
-    /**
-     * Create or retrieve a format instance.
-     *
-     * @param name The format type name
-     * @param arguments Arguments used to create the format instance. This allows the
-     *                  {@code FormatFactory} to implement the "format style"
-     *                  concept from {@code java.text.MessageFormat}.
-     * @param locale The locale, may be null
-     * @return The format instance
-     */
-    Format getFormat(String name, String arguments, Locale locale);
 
 }
